e38dd9fbf51bd180001b64fa3e5b0f91a99eb746
koundalritesh74-wq <koundalritesh74@gmail.com>
2025-12-10 06:50:05 +0530

    upload

---
+++ Practical 3/app.js
@@ -0,0 +1,7 @@
+const { add, subtract } = require('Practical 3/Math.js');
+
+const sum = add(10, 5);
+const difference = subtract(10, 5);
+
+console.log(`The sum of 10 and 5 is: ${sum}`);
+console.log(`The difference of 10 and 5 is: ${difference}`);
